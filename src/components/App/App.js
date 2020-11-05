2fc41aabfeb74ac9c5f6b9c6a287de5779a52bc3
Christian Dungca <cdungca14@gmail.com>
2020-11-05 20:07:22 +0000

    Created a simple folder structure for the project
    Created a folder structure for the project based on components. Each component will be have its own folder containing a .js and .module.scss file. The App component is the top level component (besides Index) and is rendering Navigation and Home.

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from '../Home/Home';
+import Navigation from '../Navigation/Navigation';
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Landing from '../Landing/Landing';
+import About from '../About/About';
+import Products from '../Products/Products';
+
+const Home = () => {
+    return (
+        <div>
+            <Landing />
+            <About />
+            <Products /> 
+        </div>
+        
+    )
+}
+
+export default Home; 
