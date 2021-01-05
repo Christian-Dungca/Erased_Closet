200fdd1d87978265592875e9652550e4289e7b54
Christian-Dungca <cdungca14@gmail.com>
2021-01-05 22:09:36 +0000

    Using multiple reducers
    Using combineReducer method from Redux to use a reducer for the products in bag, and list of all products

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 import "./index.css";
 import App from "./components/App/App";
 import productsReducer from "./store/reducers/products";
+import bagReducer from "./store/reducers/bag";
+
+const rootReducer = combineReducers({
+  bag: bagReducer,
+  products: productsReducer
+})
 
 const store = createStore(
-  productsReducer,
+  rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
